1853303d3f1bccfd0aae626ff54dbd76f5d63c0d
paulotijero <paulotijero@gmail.com>
2019-05-13 23:26:02 -0500

    Refactor test documentation

--- spec/hi_world_spec.rb
+++ spec/hi_world_spec.rb
@@ -1,7 +1,7 @@
 require 'hi_world'
 
-RSpec.describe 'Hi_world' do
-  it 'return hi_world when call the function'  do
+RSpec.describe 'Hi World' do
+  it 'return ~Hi world!!~ when call the function'  do
     hi = HiWorld.hi
     expect(hi).to eq("Hi world!!")
   end
